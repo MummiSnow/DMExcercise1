2094eb76fa15d4c49ef26d2a5bfe00ba9930c67b
Anders Geer Jakobsen <geer.anders@gmail.com>
2016-11-12 15:14:38 +0100

    Added Cliche CLI tool jar file. Added few commands to main, currently commented out to allow Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,51 @@
+import asg.cliche.Command;
+import asg.cliche.Param;
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
+
+import java.util.Collection;
 
 public class Main {
-	
-	
+
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
-	
+/*
+	@Command(description = "Compares leaf and parent branch")
+	public void checkParent(@Param(name = "leaf") String leaf,
+							@Param(name = "branch") String father)
+
+	{
+		if (leaf.trim().length() == 0 && father.trim().length() != 0) {
+			Collection<Component> leafs = getLeafs(father.trim());
+			System.out.println(leafs);
+		} else if (father.trim().length() == 0 && leaf.trim().length() != 0) {
+			Component father = leaf.getFather();
+			System.out.println(father);
+		} else {
+			return (father == leaf.getFather());
+		}
+	}
+
+	@Command(description = "Compares leaf and grandparent branch")
+	public void checkGrandparent(@Param(name = "leaf") String leaf,
+								 @Param(name = "branch") String grandfather)
+
+	{
+		Collection<Component> leafs = new arrayList<Component>;
+		if (leaf.trim().length() == 0 && grandfather.trim().length() != 0) {
+			for (Component father : grandfather.getLeafs()) {
+				leafs += father.getLeafs();
+			}
+			System.out.println(leafs);
+		} else if (grandfather.trim().length() == 0 && leaf.trim().length() != 0) {
+			Component gfather = leaf.getFather().getFather();
+			System.out.println(gfather);
+		} else {
+			return (grandfather == leaf.getFather().getFather());
+		}
+	}
+
+*/
 }
